fd368f61712b1c5d4dca57438a8bb6e712b1731b
LENOVO <imbakito1604@gmail.com>
2022-06-07 10:52:13 +0700

    them file

---
+++ tuan2/baitap1.js
@@ -0,0 +1,6 @@
+let ly=parseInt(prompt("nhap diem ly"))
+let toan=parseInt(prompt("nhap diem toan"))
+let hoa=parseInt(prompt("nhap diem hoa"))
+let area = (toan+ly+hoa)/3
+document.write('diem trung binh:' +area)
+
